455df064a20522fd750984741721e80ed8b7138f
Krys Lawrence <krys@krys.ca>
2020-04-15 20:12:13 -0400

    Added ImplicitPackageResolver class.
    This makes operationId entries in openapi.yaml nicer and less
    implementation-dependent.

--- racecard/server/rest/app.py
+++ racecard/server/rest/app.py
@@ -19,12 +19,50 @@
 
 
 from connexion import FlaskApp
+from connexion.resolver import Resolution, Resolver
 
 app = FlaskApp(__name__, specification_dir="openapi/")
-app.add_api("openapi.yaml", strict_validation=True, validate_responses=True)
+
+
+class ImplicitPackageResolver(Resolver):
+    """Resolver that uses an implicit base package to resolve operationId functions.
+
+    This basically provides and implied router_controller value.
+
+    This means operationId becomes required, but the full package path does not need to
+    be included in openapi spec file, since, frankly, that is an implementation detail
+    and not really part of the interface.
+
+    operationId should be just that, and ID, not necessarily an absolute path in to the
+    system.
+    """
+
+    def __init__(self, base_package_name):
+        self._base_package_name = base_package_name
+        super().__init__()
+
+    def resolve(self, operation):
+        """Resolve operation by pre-pending the base package name.
+
+        Also keeps the base package name out of the routing table so that Flask's
+        url_for(), for example, can be used would including all the base package parts.
+
+        E.g. url_for(".games_search") instead of
+             url_for(".racecard_servers_rest_api_games_search")
+        """
+        operation_id = self.resolve_operation_id(operation)
+        full_operation_id = f"{self._base_package_name}.{operation_id}"
+        function = self.resolve_function_from_operation_id(full_operation_id)
+        return Resolution(function, operation_id)
 
 
 def main():
+    app.add_api(
+        "openapi.yaml",
+        strict_validation=True,
+        validate_responses=True,
+        resolver=ImplicitPackageResolver("racecard.server.rest.api"),
+    )
     app.run()
 
 
